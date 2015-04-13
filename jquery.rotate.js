e8a64960690240eeff70c36a7c80def3f768a526
mgalante <mgalante@gmail.com>
2015-04-13 12:48:33 -0300

    Add License

--- jquery.rotate.js
+++ jquery.rotate.js
@@ -1,10 +1,15 @@
+/**
+ * Copyright (c) 2015 Miguel Galante
+ * Licensed under the MIT license
+ */
+
 jQuery.fn.rotate = function(degree)
 {
 	return this.each(function(){
 		$(this).css({
 	 		"-ms-transform": "rotate(" + degree +  "deg)", /* IE 9 */
-        	"-webkit-transform": "rotate(" + degree +  "deg)",/* Chrome, Safari, Opera */
-        	"transform": "rotate(" + degree +  "deg)"
+        		"-webkit-transform": "rotate(" + degree +  "deg)",/* Chrome, Safari, Opera */
+        		"transform": "rotate(" + degree +  "deg)"
 		});
 	});
 };
